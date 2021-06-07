e72bc688f4b3643df8755138e7fa5cb4c22d2420
João Martins <45777813+xpto051@users.noreply.github.com>
2021-06-07 20:30:25 +0100

    bottom navbar

---
+++ lib/screens/home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:trainees_flutter/screens/profile.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  List<String> get _appBarTitle => [
+    "Home",
+    "Subscribe to gym",
+    "Profile"
+  ];
+
+  List<Widget> get _children => [
+        Profile(Colors.red), // home
+        Profile(Colors.green), // QRCode
+        Profile(Colors.black) //Profile
+      ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(_appBarTitle[_currentIndex])),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.qr_code), label: "Subscribe"),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
